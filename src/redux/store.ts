a657d9055356553a9f4fa33b0cb5bb88b7c35654
Nfemz <femianicholas@gmail.com>
2019-06-20 01:27:54 -0700

    Moved redux-devtools from devdep -> dep due to Heroku issue

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,9 @@
 // Redux imports
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import getUsersReducer from "./reducers";
 
 // Store creation
-const store = createStore(
-  getUsersReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(getUsersReducer, applyMiddleware(thunk));
 
 export default store;
